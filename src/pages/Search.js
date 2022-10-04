6513771a4c016810b6439978e84d28de88a92a66
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2022-10-04 09:41:25 -0300

    resolução do requisito 5 - form de busca e habilitação condicional do botão

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,53 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isButtonDisabled: true,
+      artistInput: '',
+    };
+  }
+
+  enableSearchButton = () => {
+    const { artistInput } = this.state;
+    const two = 2;
+    const typedLenght = artistInput.length < two;
+    this.setState({ isButtonDisabled: typedLenght });
+  };
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({
+      [name]: value,
+    }, () => this.enableSearchButton()); // só é executada após estado ser atualizado
+  };
+
   render() {
+    const { artistInput, isButtonDisabled } = this.state;
+
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <label htmlFor="name-input">
+            <input
+              data-testid="search-artist-input"
+              type="text"
+              value={ artistInput }
+              name="artistInput"
+              onChange={ this.handleChange }
+            />
+            <button
+              data-testid="search-artist-button"
+              type="button"
+              name="search"
+              disabled={ isButtonDisabled }
+            >
+              Pesquisar
+            </button>
+          </label>
+        </form>
       </div>
     );
   }
