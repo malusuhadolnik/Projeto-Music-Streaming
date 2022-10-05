7603974eaa04d7d0e43e04a5fce255bd4a928ed0
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2022-10-05 11:10:57 -0300

    nova tentativa de submissão do requisito 8

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { addSong } from '../services/favoriteSongsAPI';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 class MusicCard extends React.Component {
   constructor() {
